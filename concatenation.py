277ca6a9314117e4290b68b3a2fd53b9b966d285
Aditi6-20 <aditinag2002@gmail.com>
2024-09-02 18:22:10 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,12 @@
+first="Aditi"
+last="Nagdewete"
+fullname=first+" "+last
+print(fullname.lower())
+print(fullname.capitalize())
+
+
+#print(fullname.lower())
+#print(fullname.swapcase())
+#first="Aditi"
+#last="Nagdewete"
+#print(first,last)
---
+++ day4task2.py
@@ -0,0 +1,8 @@
+num=int(input("Enter a number\n"))
+if num>0:
+    print(f"{num} it is a postive Number")
+else :
+    if num==0:
+        print("zero")
+    else:   
+        print(f"{num} it is a negative number")
